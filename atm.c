2c785b9ed2b64de3fbd6975b8b9aac32c830737a
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2021-01-22 11:50:19 +0530

    Create atm.c

---
+++ atm.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int wd;
+	float bal;
+    scanf("%d",&wd);
+    scanf("%f",&bal);
+	if (wd%5==0 && wd<=bal-0.50){
+	    bal=bal-wd-0.5;
+	    printf("%f",bal);
+	}
+	else{
+	    printf("%f",bal);
+	}
+	
+	return 0;
+
+}
+
