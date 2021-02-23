1a4f6241d67f09c9a6ec7b29183016fb1cc891e4
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2021-02-23 18:10:25 +0530

    Create sum_or_differance.c

---
+++ sum_or_differance.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int N1,N2;
+    scanf("%d",&N1);
+    scanf("%d",&N2);
+    if(N1>N2)
+    {
+     printf("%d",N1-N2); 
+     }
+     else
+     printf("%d",N1+N2);
+	return 0;
+}
